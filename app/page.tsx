9e14732d3b370e08035b234d70204ed2a29ce842
Fiona Y. <fyeung21@gmail.com>
2026-02-25 15:43:59 -0800

    feat: style hero on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 export default function Home() {
   return (
-    <section>
-      <p>
-        "Hi there! My name is Fiona and I am a front-end web developer who takes great interest in
-        learning how to write clear and efficient modular code. Please hover over and click on the
-        thumbnails below to check out my projects!"
-      </p>
+    <section className="m-4">
+      <section className="my-10 p-4 sm:w-120 sm:m-auto sm:p-6">
+        <p>
+          "Hi there! My name is Fiona and I am a front-end web developer who takes great interest in
+          learning how to write clear and efficient modular code. Please hover over and click on the
+          thumbnails below to check out my projects!"
+        </p>
+      </section>
       <section>{"map list of projects here"}</section>
       <section>{"map list of skills here"}</section>
     </section>
