68361e9c50a899922c8c2fcdf5e7c758323f5f31
Fiona Y. <fyeung21@gmail.com>
2026-02-24 18:42:26 -0800

    feat: sketch home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <section>
+      <p>
+        "Hi there! My name is Fiona and I am a front-end web developer who takes great interest in
+        learning how to write clear and efficient modular code. Please hover over and click on the
+        thumbnails below to check out my projects!"
+      </p>
+      <section>{"map list of projects here"}</section>
+      <section>{"map list of skills here"}</section>
+    </section>
+  );
 }
