89cc6cf8c96d01969c9531054878d0c8d6c84c82
Fiona Y. <fyeung21@gmail.com>
2026-02-25 16:26:49 -0800

    feat: sketch contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+export default function Contact() {
+  return (
+    <section className="m-4">
+      <h2>contact me</h2>
+      <p>
+        Filling this form will send an email to me. You may also reach me through{" "}
+        <a href="https://ca.linkedin.com/">linkedin</a> or{" "}
+        <a href="https://github.com/fyeung21">github</a>!
+      </p>
+      <p>contact form here</p>
+    </section>
+  );
+}
