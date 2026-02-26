38cdec5bd9b14b11743051db55939a2714d2ca4c
Fiona Y. <fyeung21@gmail.com>
2026-02-25 16:35:58 -0800

    feat: add hover styles to contact page external links

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,8 +4,14 @@ export default function Contact() {
       <h2 className="text-2xl my-4">contact me</h2>
       <p>
         Filling this form will send an email to me. You may also reach me through{" "}
-        <a href="https://ca.linkedin.com/">linkedin</a> or{" "}
-        <a href="https://github.com/fyeung21">github</a>!
+        <a href="https://ca.linkedin.com/" className="font-bold hover:underline">
+          linkedin
+        </a>{" "}
+        or{" "}
+        <a href="https://github.com/fyeung21" className="font-bold hover:underline">
+          github
+        </a>
+        !
       </p>
       <article className="my-4">
         <p>contact form here</p>
